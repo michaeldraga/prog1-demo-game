e2b8dadff3a7c8d78bb6ac17d82bce422ba898bd
michaelb00m <midall01.md@gmail.com>
2021-04-18 17:16:59 +0200

    Extracts Door, Player, implements star, score

--- app/src/main/java/prog1/game/GamePanel.java
+++ app/src/main/java/prog1/game/GamePanel.java
@@ -1,50 +1,114 @@
 package prog1.game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int DELAY = 20;
-    static final int SCREEN_WIDTH = 500;
+    static final int MOVE_DELAY = 500;
+    static final int SCREEN_WIDTH = 580;
     static final int SCREEN_HEIGHT = 500;
     static final int UNIT_SIZE = 50;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int COLUMNS = 10;
     static final int ROWS = 10;
-    static final int PLAYER_SIZE = 40;
-    static final int start = UNIT_SIZE / 2 - PLAYER_SIZE / 2;
     static final Color COLOR_SQUARE = Color.black;
-    static final Color COLOR_PLAYER = Color.blue;
     static final Color COLOR_DOOR = Color.red;
+    static final String PLAYER_IMG_PATH = "app/src/main/resources/player.png";
+    static final String STAR_IMG_PATH = "app/src/main/resources/star.png";
+    final Image starImg;
+    final Image playerImg;
+    private java.util.List<Star> stars = new ArrayList<>();
+    final List<Move> moves = new ArrayList<>();
+    private Button startStopButton;
+    private Label label;
+
+    long lastMove = 0;
 
     boolean running = false;
-
-    int xOff, yOff = 0;
+    private Player player;
+    private Door door;
+    int score = 0;
 
     Timer timer;
-    Graphics g;
 
     GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.WHITE);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        this.initButton();
+        this.initLabel();
+        this.setLayout(null);
         this.startGame();
+        this.setScore();
+        this.player = new Player();
+        this.door = new Door(9, 4);
+        this.playerImg = readImage(PLAYER_IMG_PATH);
+        this.starImg = readImage(STAR_IMG_PATH);
+        new Sketch(moves);
+        this.setStars(10);
     }
 
-    private void drawSquare(Graphics g, int x, int y, int size) {
-        g.drawRect(x - size / 2, y - size / 2, size, size);
+    private void initLabel() {
+        label = new Label("");
+        label.setBounds(500, 30, 70, 20);
+        this.add(label);
+    }
+
+    private void initButton() {
+        startStopButton = new Button("Play");
+        startStopButton.setBounds(500, 0, 50, 20);
+        startStopButton.addActionListener(e -> startStopMoves());
+        this.add(startStopButton);
+    }
+
+    private void setStars(int nOfStars) {
+        while (stars.size() < nOfStars) {
+            Star newStar = new Star();
+            if (stars.stream().anyMatch(s -> Star.positionEquals(s, newStar)) ||
+                Star.positionEquals(newStar, player)) {
+                continue;
+            }
+            stars.add(newStar);
+        }
+    }
+
+    private void setScore() {
+        label.setText("Score: " + score);
+    }
+
+    public static Image readImage(String path) {
+        File imgFile = new File(path);
+        BufferedImage img;
+        try {
+            img = ImageIO.read(imgFile);
+        } catch (IOException e) {
+            System.out.println("Couldn't read image. Exiting.");
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
+        return img;
     }
 
     public void startGame() {
-        running = true;
         timer = new Timer(DELAY, this);
         timer.start();
+    }
+
+    public void startStopMoves() {
+        running = !running;
     }
 
     public void paintComponent(Graphics g) {
@@ -54,32 +118,18 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         paintSquares(g);
-        paintPlayer(g);
-    }
-
-    private void paintPlayer(Graphics g) {
-        g.setColor(COLOR_PLAYER);
-        g.fillOval(start + UNIT_SIZE * xOff, start + UNIT_SIZE * yOff, PLAYER_SIZE - 1, PLAYER_SIZE - 1);
+        door.paint(g);
+        stars.forEach(s -> s.paint(g));
+        player.paint(g);
     }
 
     public void paintSquares(Graphics g) {
         g.setColor(COLOR_SQUARE);
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (j == 9 && i == 4) {
-                    paintDoor(g, i, j);
-                    continue;
-                }
                 g.fillRect(UNIT_SIZE * j + 1, UNIT_SIZE * i + 1, UNIT_SIZE - 2, UNIT_SIZE - 2);
             }
         }
-    }
-
-    private void paintDoor(Graphics g, int i, int j) {
-        Color lastColor = g.getColor();
-        g.setColor(COLOR_DOOR);
-        g.fillRect(UNIT_SIZE * j + 1, UNIT_SIZE * i + 1, UNIT_SIZE - 2, UNIT_SIZE - 2);
-        g.setColor(lastColor);
     }
 
     public void gameOver(Graphics g) {
@@ -89,16 +139,22 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (running) {
-            checkBounds();
+            if (System.currentTimeMillis() - lastMove > MOVE_DELAY) {
+                System.out.println("help");
+                lastMove = System.currentTimeMillis();
+                Move nextMove = moves.remove(0);
+                System.out.println(nextMove.x + " " + nextMove.y);
+                player.move(nextMove.x, nextMove.y);
+                if (moves.size() == 0) running = false;
+            }
+        }
+        Optional<Star> overlapped = stars.stream().filter(s -> Star.positionEquals(s, player)).findAny();
+        if (overlapped.isPresent()) {
+            stars.remove(overlapped.get());
+            score += 1;
+            setScore();
         }
         repaint();
-    }
-
-    private void checkBounds() {
-        if (xOff >= COLUMNS) xOff = COLUMNS - 1;
-        if (xOff < 0) xOff = 0;
-        if (yOff >= ROWS) yOff = ROWS - 1;
-        if (yOff < 0) yOff = 0;
     }
 
     public class MyKeyAdapter extends KeyAdapter {
@@ -106,16 +162,16 @@ public class GamePanel extends JPanel implements ActionListener {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_DOWN:
-                    yOff += 1;
+                    player.moveY(1);
                     break;
                 case KeyEvent.VK_UP:
-                    yOff -= 1;
+                    player.moveY(-1);
                     break;
                 case KeyEvent.VK_LEFT:
-                    xOff -= 1;
+                    player.moveX(-1);
                     break;
                 case KeyEvent.VK_RIGHT:
-                    xOff += 1;
+                    player.moveX(1);
                     break;
             }
         }
