740a7c6e9aeb76d009f463bfa13fde40997737bc
michaelb00m <midall01.md@gmail.com>
2021-04-18 21:30:39 +0200

    Implements position attribute

--- app/src/main/java/prog1/game/Door.java
+++ app/src/main/java/prog1/game/Door.java
@@ -7,23 +7,24 @@ import static prog1.game.GamePanel.COLOR_DOOR;
 import static prog1.game.GamePanel.UNIT_SIZE;
 
 public class Door {
-    int x,y;
+    Position position;
 
     public Door(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.position = new Position(x,y);
     }
 
     public Door() {
         Random r = new Random();
-        this.x = r.nextInt(GamePanel.COLUMNS);
-        this.y = r.nextInt(GamePanel.ROWS);
+        this.position = new Position(
+                r.nextInt(GamePanel.COLUMNS),
+                r.nextInt(GamePanel.ROWS)
+        );
     }
 
     public void paint(Graphics g) {
         Color lastColor = g.getColor();
         g.setColor(COLOR_DOOR);
-        g.fillRect(UNIT_SIZE * x + 1, UNIT_SIZE * y + 1, UNIT_SIZE - 2, UNIT_SIZE - 2);
+        g.fillRect(UNIT_SIZE * position.x + 1, UNIT_SIZE * position.y + 1, UNIT_SIZE - 2, UNIT_SIZE - 2);
         g.setColor(lastColor);
     }
 }
