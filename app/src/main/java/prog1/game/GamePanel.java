fdc11bf4700fa42fcbdc0e58f25cdaf832fd58a7
michaelb00m <midall01.md@gmail.com>
2021-04-18 21:32:37 +0200

    Implements game over, adapts to Position introduction

--- app/src/main/java/prog1/game/GamePanel.java
+++ app/src/main/java/prog1/game/GamePanel.java
@@ -30,11 +30,13 @@ public class GamePanel extends JPanel implements ActionListener {
     final List<Move> moves = new ArrayList<>();
     private Button startStopButton;
     private Button resetButton;
-    private Label label;
+    private Label scoreLabel;
+    private JLabel gameOverLabel;
 
     long lastMove = 0;
 
     boolean running = false;
+    boolean gameOver = false;
     private final Player player;
     private final Door door;
     int score = 0;
@@ -49,19 +51,29 @@ public class GamePanel extends JPanel implements ActionListener {
         this.addKeyListener(new MyKeyAdapter());
         this.initStartStopButton();
         this.initResetButton();
-        this.initLabel();
-        this.startGame();
+        this.initScoreLabel();
+        this.initGameOverLabel();
         this.setScore();
         this.player = new Player();
         this.door = new Door(9, 4);
         this.setStars(N_OF_STARS);
         new Sketch(moves);
+        this.startGame();
     }
 
-    private void initLabel() {
-        label = new Label("");
-        label.setBounds(500, 60, 70, 20);
-        this.add(label);
+    private void initScoreLabel() {
+        scoreLabel = new Label("");
+        scoreLabel.setBounds(500, 60, 70, 20);
+        this.add(scoreLabel);
+    }
+
+    private void initGameOverLabel() {
+        gameOverLabel = new JLabel("You won!");
+        gameOverLabel.setBounds(150, 200, 200, 100);
+        gameOverLabel.setHorizontalAlignment(JLabel.CENTER);
+        Font labelFont = gameOverLabel.getFont();
+        gameOverLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 42));
+        gameOverLabel.setForeground(Color.white);
     }
 
     private void initStartStopButton() {
@@ -79,6 +91,9 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     private void resetEverything() {
+        gameOver = false;
+        running = false;
+        this.remove(this.gameOverLabel);
         player.reset();
         stars.clear();
         moves.clear();
@@ -86,14 +101,15 @@ public class GamePanel extends JPanel implements ActionListener {
         new Sketch(moves);
         score = 0;
         setScore();
+        this.grabFocus();
     }
 
     private void setStars(int nOfStars) {
         while (stars.size() < nOfStars) {
             Star newStar = new Star();
-            if (stars.stream().anyMatch(s -> Star.positionEquals(s, newStar)) ||
-                Star.positionEquals(newStar, player) ||
-                Star.positionEquals(newStar, door)) {
+            if (stars.stream().anyMatch(s -> Position.equals(s.position, newStar.position)) ||
+                    Position.equals(newStar.position, player.position) ||
+                    Position.equals(newStar.position, door.position)) {
                 continue;
             }
             stars.add(newStar);
@@ -101,7 +117,7 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     private void setScore() {
-        label.setText("Score: " + score);
+        scoreLabel.setText("Score: " + score);
     }
 
     public static Image readImage(String path) {
@@ -125,6 +141,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void startStopMoves() {
         running = !running;
+        lastMove = System.currentTimeMillis();
     }
 
     public void paintComponent(Graphics g) {
@@ -137,6 +154,7 @@ public class GamePanel extends JPanel implements ActionListener {
         door.paint(g);
         stars.forEach(s -> s.paint(g));
         player.paint(g);
+        if (gameOver) gameOver(g);
     }
 
     public void paintSquares(Graphics g) {
@@ -149,26 +167,32 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g) {
-
+        g.setColor(new Color(0, 0, 0, 200));
+        g.fillRect(0, 0, 500, 500);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (running) {
-            if (System.currentTimeMillis() - lastMove > MOVE_DELAY) {
-                System.out.println("help");
-                lastMove = System.currentTimeMillis();
-                Move nextMove = moves.remove(0);
-                System.out.println(nextMove.x + " " + nextMove.y);
-                player.move(nextMove.x, nextMove.y);
-                if (moves.size() == 0) running = false;
+        if (!gameOver) {
+            if (running) {
+                if (System.currentTimeMillis() - lastMove > MOVE_DELAY) {
+                    lastMove = System.currentTimeMillis();
+                    Move nextMove = moves.remove(0);
+                    player.move(nextMove.x, nextMove.y);
+                    if (moves.size() == 0) running = false;
+                }
             }
-        }
-        Optional<Star> overlapped = stars.stream().filter(s -> Star.positionEquals(s, player)).findAny();
-        if (overlapped.isPresent()) {
-            stars.remove(overlapped.get());
-            score += 1;
-            setScore();
+            Optional<Star> overlapped = stars.stream().filter(s -> Position.equals(s.position, player.position)).findAny();
+            if (overlapped.isPresent()) {
+                stars.remove(overlapped.get());
+                score += 1;
+                setScore();
+            }
+            if (stars.size() == 0 && Position.equals(door.position, player.position)) {
+                running = false;
+                gameOver = true;
+                this.add(gameOverLabel);
+            }
         }
         repaint();
     }
@@ -176,19 +200,21 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_DOWN:
-                    player.moveY(1);
-                    break;
-                case KeyEvent.VK_UP:
-                    player.moveY(-1);
-                    break;
-                case KeyEvent.VK_LEFT:
-                    player.moveX(-1);
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    player.moveX(1);
-                    break;
+            if (!gameOver) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_DOWN:
+                        player.moveY(1);
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.moveY(-1);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        player.moveX(-1);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.moveX(1);
+                        break;
+                }
             }
         }
     }
