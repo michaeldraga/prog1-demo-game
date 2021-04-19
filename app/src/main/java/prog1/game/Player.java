677bf612d9ffc48f8bd442df14a94c7c5b34e4d3
michaelb00m <midall01.md@gmail.com>
2021-04-19 12:38:25 +0200

    Implements moves, simMoves, doorPosition methods

--- app/src/main/java/prog1/game/Player.java
+++ app/src/main/java/prog1/game/Player.java
@@ -2,6 +2,8 @@ package prog1.game;
 
 import java.awt.*;
 import java.awt.image.ImageObserver;
+import java.util.ArrayList;
+import java.util.List;
 
 import static prog1.game.GamePanel.*;
 
@@ -10,6 +12,8 @@ public class Player {
     static final int SIZE = 40;
     static final int START = UNIT_SIZE / 2 - SIZE / 2;
     Position position;
+    Position doorPosition;
+    final List<Position> moves = new ArrayList<>();
 
     final Image img;
 
@@ -43,6 +47,65 @@ public class Player {
         this.position.y = restrict(this.position.y + y, 0, ROWS);
     }
 
+    public void move(Position pos) {
+        this.position.x = restrict(this.position.x + pos.x, 0, COLUMNS);
+        this.position.y = restrict(this.position.y + pos.y, 0, ROWS);
+    }
+
+    public boolean isExitInRowRight() {
+        return position.x < doorPosition.x && position.y == doorPosition.y;
+    }
+
+    public boolean isExitInRowLeft() {
+        return position.x > doorPosition.x && position.y == doorPosition.y;
+    }
+
+    public boolean isExitInColumnDown() {
+        return position.y < doorPosition.y && position.x == doorPosition.x;
+    }
+
+    public boolean isExitInColumnUp() {
+        return position.y > doorPosition.y && position.x == doorPosition.x;
+    }
+
+    private void simMoveX(int amount) {
+        moveX(amount);
+        moves.add(new Position(amount, 0));
+    }
+
+    private void simMoveY(int amount) {
+        moveY(amount);
+        moves.add(new Position(0, amount));
+    }
+
+    public void right() {
+        simMoveX(1);
+    }
+
+    public void left() {
+        simMoveX(-1);
+    }
+
+    public void down() {
+        simMoveY(1);
+    }
+
+    public void up() {
+        simMoveY(-1);
+    }
+
+    public void nextMove() {
+        move(moves.remove(0));
+    }
+
+    public boolean movesDone() {
+        return moves.isEmpty();
+    }
+
+    public boolean isOnExit() {
+        return Position.equals(position, doorPosition);
+    }
+
     private static int restrict(int val, int min, int max) {
         if (val < min) return min;
         if (val > max - 1) return max - 1;
@@ -51,5 +114,13 @@ public class Player {
 
     public void reset() {
         position = new Position(0, 0);
+    }
+
+    public void resetMoves() {
+        moves.clear();
+    }
+
+    public void setDoorPosition(Position doorPosition) {
+        this.doorPosition = doorPosition;
     }
 }
