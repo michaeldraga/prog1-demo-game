bfc64038aa4d2e92620abc707c01e0f028e64419
michaelb00m <midall01.md@gmail.com>
2021-04-19 20:34:30 +0200

    Adds proxy methods for player class methods

--- app/src/main/java/prog1/game/Sketch.java
+++ app/src/main/java/prog1/game/Sketch.java
@@ -2,7 +2,64 @@ package prog1.game;
 
 public class Sketch {
 
-    public static void sketch(Player player) throws MaxMovesException {
+    Player player;
 
+    public Sketch(Player player) {
+        this.player = player;
+    }
+
+    private void up() throws MaxMovesException {
+        player.up();
+    }
+
+    private void down() throws MaxMovesException {
+        player.down();
+    }
+
+    private void right() throws MaxMovesException {
+        player.right();
+    }
+
+    private void left() throws MaxMovesException {
+        player.left();
+    }
+
+    private boolean isExitRight() throws  MaxMovesException {
+        return player.isExitRight();
+    }
+
+    private boolean isExitLeft() throws  MaxMovesException {
+        return player.isExitLeft();
+    }
+
+    private boolean isExitUp() throws  MaxMovesException {
+        return player.isExitUp();
+    }
+
+    private boolean isExitDown() throws  MaxMovesException {
+        return player.isExitDown();
+    }
+
+    private boolean isOnExit() {
+        return player.isOnExit();
+    }
+
+    private boolean won() {
+        return player.won;
+    }
+
+    public void sketch() throws MaxMovesException {
+        while (!isExitRight() && !isExitLeft() && !isOnExit()) {
+            down();
+        }
+        if (isExitRight()) {
+            while (!isOnExit()) {
+                right();
+            }
+        } else {
+            while (!isOnExit()) {
+                left();
+            }
+        }
     }
 }
