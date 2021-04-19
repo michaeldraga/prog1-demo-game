34d90fdf964b3843cd3cfbce416a1e0784f818ec
michaelb00m <midall01.md@gmail.com>
2021-04-19 12:38:51 +0200

    Extracts moves and sim methods

--- app/src/main/java/prog1/game/Sketch.java
+++ app/src/main/java/prog1/game/Sketch.java
@@ -1,44 +1,15 @@
 package prog1.game;
 
-import java.util.List;
-
 public class Sketch {
 
-    List<Move> moves;
+    Player player;
 
-    public Sketch(List moves) {
-        this.moves = moves;
+    public Sketch(Player player) {
+        this.player = player;
         sketch();
     }
 
-    private void moveX(int amount) {
-        moves.add(new Move(amount, 0));
-    }
-
-    private void moveY(int amount) {
-        moves.add(new Move(0, amount));
-    }
-
-    private void right() {
-        moveX(1);
-    }
-
-    private void left() {
-        moveX(-1);
-    }
-
-    private void down() {
-        moveY(1);
-    }
-
-    private void up() {
-        moveY(-1);
-    }
-
     public void sketch() {
-        for (int i = 0; i < 4; i++) {
-            down();
-            right();
-        }
+
     }
 }
